a01955e968a8434333122472b101188efdb3e7ad
Babnik21 <jure.babnik.21@gmail.com>
2020-12-09 16:45:35 +0100

    funkcije, ki racunajo vse iskane podatke

--- graphs.py
+++ graphs.py
@@ -3,6 +3,7 @@ import graph
 from graph.visuals import plot_3d, plot_2d
 import math
 import random
+import pandas
 
 graf = Graph()
 graf.add_node("a")
@@ -133,8 +134,10 @@ def average_efficiency(graph):
 
 
 def global_efficiency(graph, ideal):
-    return average_efficiency(graph)/average_efficiency(ideal)
-
+    try:
+        return average_efficiency(graph)/average_efficiency(ideal)
+    except ZeroDivisionError:
+        return 0
 
 def local_efficiency(graph):
     total = 0
@@ -167,7 +170,6 @@ def average_sim(graph, percent):
         second = random.choice(graph.nodes())
         if first != second:
             pairs.add((first, second))
-    print("sem ven iz loopa")
     if lay:
         pairs = all_pairs - pairs
     total = 0
@@ -252,6 +254,105 @@ def remove_centered(graph, percent, start = None):
     return resitev
 
 
+# Naredi csv file
 
-        
-        
+
+
+
+# Sim povprečje za vse (velike) grafe
+columns = ["Percent", "Avg. efficiency", "Sim 10", "Sim 100", "Sim 1000"]
+columns_removed = ["Percent", "Random", "Centered"]
+data = {}                   # Za sim
+
+# Grid
+for i in [2, 3, 4, 5, 10, 20, 50, 100]:                                            # Spremeni stevilke
+    for j in [1, 2, 3, 4, 5, 10, 20, 50, 100]:
+        for k in [1, 2, 3, 4, 5, 10, 20, 50, 100]:
+            # sim zanka
+            for percent in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:                     # Spremeni stevilke
+                grid_data = []
+                grid_global = []
+                grid_centered = []
+                for _ in range(1000):            #
+                    grid = generate_3d_grid(i, j, k)
+                    grid_1 = remove_random(generate_3d_grid(i,j,k), percent)
+                    grid_2 = remove_centered(generate_3d_grid(i, j, k), percent)
+                    grid_data.append(average_sim(grid, percent))
+                    grid_global.append(global_efficiency(grid_1, grid))
+                    grid_centered.append(global_efficiency(grid_2, grid))
+                data["Grid {}x{}x{} at {}".format(i, j, k, percent)] = [percent, average_efficiency(grid), sum(grid_data[:10])/10, sum(grid_data[:100])/100, sum(grid_data)/1000]
+                data_removed["Grid {}x{}x{} at {}".format(i, j, k, percent)] = [percent, sum(grid_global)/1000, sum(grid_centered)/1000]
+
+# Tree
+for i in range(2, 21): 
+    tree = generate_binary(i)
+    for percent in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:                         # Spremeni stevilke
+        tree_data = []
+        tree_global= []
+        tree_centered = []
+        for _ in range(1000):            #
+            tree_data.append(average_sim(tree, percent))
+            tree_global.append(global_efficiency(remove_random(tree, percent), tree))
+            tree_centered.append(global_efficiency(remove_centered(tree, percent), tree))
+        data["Tree {} at {}".format(i, percent)] = [percent, average_efficiency(tree), sum(tree_data[:10])/10, sum(tree_data[:100])/100, sum(tree_data)/1000]
+        data_removed["Tree {} at {}".format(i, percent)] = [percent, sum(tree_global)/1000, sum(tree_centered)/1000]
+
+# Cycle
+for i in [2, 3, 4, 5, 10, 20, 50, 100, 200, 300, 400, 500, 1000, 10000, 100000, 1000000]: 
+    cycle = generate_cycle(i)
+    for percent in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:                         # Spremeni stevilke
+        cycle_data = []
+        cycle_global= []
+        cycle_centered = []
+        for _ in range(1000):        #
+            cycle_data.append(average_sim(cycle, percent))
+            cycle_global.append(global_efficiency(remove_random(cycle, percent), cycle))
+            cycle_centered.append(global_efficiency(remove_centered(cycle, percent), cycle))
+        data["Cycle {} at {}".format(i, percent)] = [percent, average_efficiency(cycle), sum(cycle_data[:10])/10, sum(cycle_data[:100])/100, sum(cycle_data)/1000]
+        data_removed["Cycle {} at {}".format(i, percent)] = [percent, sum(cycle_global)/1000, sum(cycle_centered)/1000]
+
+
+df = pandas.DataFrame.from_dict(data, orient='index', columns = columns)
+
+
+
+
+
+
+
+# Nedelujoče povezave na gridih, drevesih in naključnih remove centered
+cols = ["Random", "Centered"]
+data_1 = {}
+data_2 = {}
+osnoven = generate_3d_grid(50, 50, 50)
+osnoven_2 = generate_3d_grid(100, 100, 100)
+for percent in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:                 # Spremeni
+    rand = remove_random(generate_3d_grid(10, 10, 10), percent)
+    centered = remove_centered(generate_3d_grid(10, 10, 10), percent)
+    data_1[percent] = [global_efficiency(rand, osnoven), global_efficiency(centered, osnoven)]
+    random_2 = remove_random(generate_3d_grid(100, 100, 100), percent)
+    centered_2 = remove_centered(generate_3d_grid(100, 100, 100), percent)
+    data_2[percent] = [global_efficiency(random_2, osnoven_2), global_efficiency(centered_2, osnoven_2)]
+
+df_1 = pandas.DataFrame.from_dict(data_1, orient='index', columns = cols)
+df_2 = pandas.DataFrame.from_dict(data_2, orient='index', columns = cols)
+
+
+data_1_bin = {}
+data_2_bin = {}
+osnoven_b = generate_binary(10)
+osnoven_2_b = generate_binary(20)
+for percent in [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]:                 # Spremeni
+    rand = remove_random(generate_binary(10), percent)
+    centered = remove_centered(generate_binary(10), percent)
+    data_1_bin[percent] = [global_efficiency(rand, osnoven_b), global_efficiency(centered, osnoven_b)]
+    random_2 = remove_random(generate_binary(20), percent)
+    centered_2 = remove_centered(generate_binary(20), percent)
+    data_2_bin[percent] = [global_efficiency(random_2, osnoven_2_b), global_efficiency(centered_2, osnoven_2_b)]
+
+df_1 = pandas.DataFrame.from_dict(data_1_bin, orient='index', columns = cols)
+df_2 = pandas.DataFrame.from_dict(data_2_bin, orient='index', columns = cols)
+
+
+
+
